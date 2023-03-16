dab081d9324cf9ac71631b278c9274dd02c25c7d
Alessandro Ontani <casualotn@gmail.com>
2023-03-16 17:07:13 +0100

    adds spacing in console

--- src/EmployeeManagement.java
+++ src/EmployeeManagement.java
@@ -6,20 +6,25 @@ public class EmployeeManagement {
         Employee e3 = new Employee("M-N", 1115, 33.60, Levels.EMPLOYEE, Departments.ADMINISTRATION);
         Employee e4 = new Employee("F-R", 1820, 38.20, Levels.MANAGER, Departments.SALES);
         Employee[] employeeList = {e1, e2, e3, e4};
+        System.out.println(""); // used as spacing in console
 
         // promote workman e2 to employee
         e2.promoteEmployee();
         System.out.println(e2.toString());
+        System.out.println(""); // used as spacing in console
 
         // promote employee e3 to director
         e3.promoteEmployee();
         System.out.println(e3.toString());
+        System.out.println(""); // used as spacing in console
 
         // print in console all Employee
         printAllEmployee(employeeList);
+        System.out.println(""); // used as spacing in console
 
         // calculate global salary of the company
         calculateGlobalSalary(employeeList);
+        System.out.println(""); // used as spacing in console
     }
 
     public static void printAllEmployee(Employee[] list) {
